3800482938b05b03ccd62d2abed7fab79f2e7efc
Fidan Hykolli <fidan-323@live.com>
2024-04-27 16:37:05 +0200

    Initial commit

---
+++ src/main/java/fidanhykolli/gjejpuneapp/GjejpuneAppApplication.java
@@ -0,0 +1,13 @@
+package fidanhykolli.gjejpuneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjejpuneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjejpuneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fidanhykolli/gjejpuneapp/GjejpuneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fidanhykolli.gjejpuneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjejpuneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
